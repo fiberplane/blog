a996dd81a202c3a58765b182b89ca28bfa7ce6ac
Oscar <33732524+oscarvz@users.noreply.github.com>
2025-11-06 15:59:05 +0100

    Add OG images (#14)
    * Add `astro-og-canvas` & set up OG img generation
    
    * Article OG tags
    
    * Update description
    
    * Use Geist font
    
    * Fix build
    
    * Add secondary color

---
+++ src/pages/og/[...blog].ts
@@ -0,0 +1,40 @@
+import { getCollection } from "astro:content";
+import { OGImageRoute } from "astro-og-canvas";
+
+const blogEntries = await getCollection("blog");
+const pages = Object.fromEntries(blogEntries.map(({ id, data }) => [id, data]));
+
+export const { getStaticPaths, GET } = OGImageRoute({
+	param: "blog",
+	pages,
+	getImageOptions: (_, page) => ({
+		title: page.title,
+		description: page.description,
+		bgImage: {
+			path: "./src/assets/og-bg.png",
+			position: "center",
+			fit: "cover",
+		},
+		logo: {
+			path: "./src/assets/fp-logo.png",
+		},
+		font: {
+			title: {
+				size: 56,
+				family: "Geist Sans",
+				weight: "Bold",
+			},
+			description: {
+				size: 32,
+				family: "Geist Sans",
+				weight: "Normal",
+				color: [138, 143, 152],
+			},
+		},
+		fonts: [
+			"./node_modules/@fontsource/geist-sans/files/geist-sans-latin-400-normal.woff2",
+			"./node_modules/@fontsource/geist-sans/files/geist-sans-latin-700-normal.woff2",
+		],
+		padding: 80,
+	}),
+});
---
+++ src/pages/og/[...route].ts
@@ -0,0 +1,44 @@
+import { OGImageRoute } from "astro-og-canvas";
+
+// Generate a default OG image for non-blog pages
+const pages = {
+	index: {
+		title: "Fiberplane Blog",
+		description: "Dive into Fiberplane's MCP integrations and developer tools.",
+	},
+};
+
+export const { getStaticPaths, GET } = OGImageRoute({
+	param: "route",
+	pages,
+	getImageOptions: (_, page) => ({
+		title: page.title,
+		description: page.description,
+		bgImage: {
+			path: "./src/assets/og-bg.png",
+			position: "center",
+			fit: "cover",
+		},
+		logo: {
+			path: "./src/assets/fp-logo.png",
+		},
+		font: {
+			title: {
+				size: 72,
+				family: "Geist Sans",
+				weight: "Bold",
+			},
+			description: {
+				size: 48,
+				family: "Geist Sans",
+				weight: "Normal",
+				color: [138, 143, 152],
+			},
+		},
+		fonts: [
+			"./node_modules/@fontsource/geist-sans/files/geist-sans-latin-400-normal.woff2",
+			"./node_modules/@fontsource/geist-sans/files/geist-sans-latin-700-normal.woff2",
+		],
+		padding: 80,
+	}),
+});
